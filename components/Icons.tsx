3e38e9d1a0738a660fa802d36f0a944af1eaea4f
solvman <solvman@gmail.com>
2024-08-07 21:28:16 -0500

    chore: Add useId hook for unique element ID in IconFrontendMentor component

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/utils/utils";
+import { useId } from "react";
 
 type IconProps = {
   width?: number;
@@ -11,6 +12,9 @@ export function IconFrontendMentor({
   height = 23,
   className,
 }: IconProps) {
+  const id = useId();
+  const elementId = `FrontendMentor-${id}`;
+
   return (
     <svg
       width="26"
@@ -18,9 +22,9 @@ export function IconFrontendMentor({
       viewBox={`0 0 ${width} ${height}`}
       className={cn("fill-white inline-block", className)}
       role="img"
-      aria-labelledby="FrontendMentor"
+      aria-labelledby={elementId}
     >
-      <title id="FrontendMentor">Frontend Mentor</title>
+      <title id={elementId}>Frontend Mentor</title>
       <path d="M13.084.23a.751.751 0 0 0-.736.75v14.267a.75.75 0 1 0 1.5 0V.98a.75.75 0 0 0-.763-.75ZM24.44 4.504a.752.752 0 0 0-.284.064l-6.44 2.875a.752.752 0 0 0 0 1.37l6.44 2.884a.75.75 0 0 0 .612-1.369L19.861 8.13l4.907-2.191a.753.753 0 0 0 .38-.99.752.752 0 0 0-.708-.444ZM1.371 9.663a.752.752 0 0 0-.74.938C2.41 17.447 8.603 22.23 15.685 22.23a.75.75 0 1 0 0-1.501A14.053 14.053 0 0 1 2.083 10.225a.75.75 0 0 0-.712-.561v-.001Z" />
     </svg>
   );
